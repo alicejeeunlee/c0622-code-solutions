af68c603a66dcd709bf0d2923a7681d1e50f667e
Alice Lee <alicejeeuncodes@gmail.com>
2022-07-06 22:49:45 +0000

    Completed javascript-functions exercise.

---
+++ javascript-functions/main.js
@@ -0,0 +1,24 @@
+function convertMinutesToSeconds(minutes) {
+  return minutes * 60;
+}
+console.log('convertMinutesToSecondsResult:', convertMinutesToSeconds(5));
+
+function greet(name) {
+  return 'Hey, ' + name;
+}
+console.log('greetResult:', greet('Beavis'));
+
+function getArea(width, height) {
+  return width * height;
+}
+console.log('getAreaResult:', getArea(17, 42));
+
+function getFirstName(person) {
+  return person.firstName;
+}
+console.log('getFirstNameResult:', getFirstName({ firstName: 'Lelouche', lastName: 'Lamperouge' }));
+
+function getLastElement(array) {
+  return array[array.length - 1];
+}
+console.log('getLastElementResult:', getLastElement(['propane', 'and', 'propane', 'accessories']));
