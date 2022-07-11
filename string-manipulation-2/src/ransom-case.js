706a6885f8c59cbfae20506b3fe334960d61f7bd
Alice Lee <alicejeeuncodes@gmail.com>
2022-07-11 18:27:09 +0000

    2nd attempt at ransom-case function.

--- string-manipulation-2/src/ransom-case.js
+++ string-manipulation-2/src/ransom-case.js
@@ -9,7 +9,9 @@ for every other character in string, starting at index 1, iterate += 2
 function ransomCase(string) {
   var cased = string[0].toLowerCase();
   for (var i = 1; i < string.length; i++) {
-    if (i % 2 === 0) {
+    if (string[i] === string[i].toUpperCase()) {
+      cased += string[i].toLowerCase();
+    } else if (i % 2 === 0) {
       cased += string[i];
     } else {
       cased += string[i].toUpperCase();
