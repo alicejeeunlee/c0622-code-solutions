a90c91973995ffa961dc7641cc1f53da9e1e20c1
Alice Lee <alicejeeuncodes@gmail.com>
2022-07-11 16:58:51 +0000

    Started string-manipulation-2 exercise.

--- string-manipulation-2/src/ransom-case.js
+++ string-manipulation-2/src/ransom-case.js
@@ -1,1 +1,19 @@
 /* exported ransomCase */
+
+/*
+for every other character in string, starting at index 1, iterate += 2
+  uppercase that character
+  return string
+*/
+
+function ransomCase(string) {
+  var cased = string[0].toLowerCase();
+  for (var i = 1; i < string.length; i++) {
+    if (i % 2 === 0) {
+      cased += string[i];
+    } else {
+      cased += string[i].toUpperCase();
+    }
+  }
+  return cased;
+}
--- string-manipulation-2/src/truncate.js
+++ string-manipulation-2/src/truncate.js
@@ -1,5 +1,10 @@
 /* exported truncate */
 
+/*
+slice(indexStart, indexEnd)
+concatenate '...'
+*/
+
 function truncate(length, string) {
   return string.slice(0, length) + '...';
 }
