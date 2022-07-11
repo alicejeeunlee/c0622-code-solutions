5ae713a01afeeafdbcd658e264b3f0b657989ab8
Alice Lee <alicejeeuncodes@gmail.com>
2022-07-11 21:32:26 +0000

    Started numVowels function

--- string-manipulation-2/src/capitalize-words.js
+++ string-manipulation-2/src/capitalize-words.js
@@ -1,1 +1,19 @@
 /* exported capitalizeWords */
+
+/*
+start with all letters lowercased
+split into each individual word (at spaces)
+store array into variable | words
+for each word
+  capitalize first letter (index: 0) and concatenate with rest of word (substring(1))
+join array of words back to string separated by space
+return string
+*/
+
+function capitalizeWords(string) {
+  var words = string.toLowerCase().split(' ');
+  for (var i = 0; i < words.length; i++) {
+    words[i] = words[i][0].toUpperCase() + words[i].substring(1);
+  }
+  return words.join(' ');
+}
--- string-manipulation-2/src/ransom-case.js
+++ string-manipulation-2/src/ransom-case.js
@@ -1,18 +1,24 @@
 /* exported ransomCase */
 
 /*
-for every other character in string, starting at index 1, iterate += 2
-  uppercase that character
-  return string
+make variable with first letter lower cased | cased
+look at each letter in string after the first
+if index is even AND is uppercased | test case QUUX
+  concatenate lower case letter at index to cased
+if index is even
+  concatenate lower case letter at index to cased
+else
+  concatenate upper case letter at index to cased
+return cased
 */
 
 function ransomCase(string) {
   var cased = string[0].toLowerCase();
   for (var i = 1; i < string.length; i++) {
-    if (string[i] === string[i].toUpperCase()) {
+    if (i % 2 === 0 && string[i] === string[i].toUpperCase()) {
       cased += string[i].toLowerCase();
     } else if (i % 2 === 0) {
-      cased += string[i];
+      cased += string[i].toLowerCase();
     } else {
       cased += string[i].toUpperCase();
     }
