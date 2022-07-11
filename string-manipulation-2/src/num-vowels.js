665fa66c3f0c95cb7258216d61264f5b396e2b4f
Alice Lee <alicejeeuncodes@gmail.com>
2022-07-11 21:34:00 +0000

    Working on numVowels function.

--- string-manipulation-2/src/num-vowels.js
+++ string-manipulation-2/src/num-vowels.js
@@ -1,1 +1,9 @@
 /* exported numVowels */
+
+/*
+variable to store count of how many vowels | initial value 0
+make string all lower cased for ease of comparison
+for each letter in string
+  if letter (string[i]) is equal to any vowel | use OR comparison
+
+*/
