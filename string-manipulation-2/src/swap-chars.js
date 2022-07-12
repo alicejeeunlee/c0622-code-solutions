ce79a84482e86383e0b926541af4d0f04efc7c76
Alice Lee <alicejeeuncodes@gmail.com>
2022-07-12 04:22:13 +0000

    Completed string-manipulation-2 exercise.

--- string-manipulation-2/src/swap-chars.js
+++ string-manipulation-2/src/swap-chars.js
@@ -1,9 +1,27 @@
 /* exported swapChars */
 
+/*
+store empty string to store swapped string | swapped
+look through each character in string
+  if index is at firstIndex
+    concatenate string[secondIndex]
+  else if index is at secondIndex
+    concatenate string[firstIndex]
+  else
+    concatenate at string[i]
+return swapped
+*/
+
 function swapChars(firstIndex, secondIndex, string) {
-  var firstChar = string[firstIndex];
-  var secondChar = string[secondIndex];
-  string.replace(string[firstIndex], secondChar);
-  string.replace(string[secondIndex], firstChar);
-  return string;
+  var swapped = '';
+  for (var i = 0; i < string.length; i++) {
+    if (i === firstIndex) {
+      swapped += string[secondIndex];
+    } else if (i === secondIndex) {
+      swapped += string[firstIndex];
+    } else {
+      swapped += string[i];
+    }
+  }
+  return swapped;
 }
