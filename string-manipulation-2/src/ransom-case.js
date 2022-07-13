3103d43665dbd86785ba90e999160d9e865b222a
Alice Lee <alicejeeuncodes@gmail.com>
2022-07-13 20:06:47 +0000

    Corrected ransomCase function logic.

--- string-manipulation-2/src/ransom-case.js
+++ string-manipulation-2/src/ransom-case.js
@@ -3,24 +3,24 @@
 /*
 make variable with first letter lower cased | cased
 look at each letter in string after the first
-if index is even AND is uppercased | test case QUUX
-  concatenate lower case letter at index to cased
-if index is even
-  concatenate lower case letter at index to cased
-else
-  concatenate upper case letter at index to cased
+  if not even
+    concatenate upperCase
+  else
+    concatenate lowerCase
 return cased
+*/
+
+/* if not even
+    cased += lower
 */
 
 function ransomCase(string) {
   var cased = string[0].toLowerCase();
   for (var i = 1; i < string.length; i++) {
-    if (i % 2 === 0 && string[i] === string[i].toUpperCase()) {
-      cased += string[i].toLowerCase();
-    } else if (i % 2 === 0) {
-      cased += string[i].toLowerCase();
-    } else {
+    if (i % 2 !== 0) {
       cased += string[i].toUpperCase();
+    } else {
+      cased += string[i].toLowerCase();
     }
   }
   return cased;
