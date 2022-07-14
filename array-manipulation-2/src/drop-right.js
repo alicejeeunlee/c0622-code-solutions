c3b7cc2548fb42996f080e9c3dc04c392a03918d
Alice Lee <alicejeeuncodes@gmail.com>
2022-07-14 19:37:27 +0000

    Solved dropRight function.

--- array-manipulation-2/src/drop-right.js
+++ array-manipulation-2/src/drop-right.js
@@ -1,1 +1,11 @@
 /* exported dropRight */
+
+/*
+slice(start, end)
+starts at 0
+ends at array.length - count
+*/
+
+function dropRight(array, count) {
+  return array.slice(0, array.length - count);
+}
