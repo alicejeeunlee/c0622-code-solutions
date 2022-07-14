585a12c66272eba75960d94edc090d1288a7c827
Alice Lee <alicejeeuncodes@gmail.com>
2022-07-14 00:38:28 +0000

    Solved includes and takeRight function.

--- array-manipulation-2/src/includes.js
+++ array-manipulation-2/src/includes.js
@@ -4,18 +4,14 @@
 look through items in array
   if items === value
     return true
-  else
-    return false
+  return false
 */
 
 function includes(array, value) {
-  var includesValue = '';
   for (var i = 0; i < array.length; i++) {
     if (array[i] === value) {
-      includesValue = true;
-    } else {
-      includesValue = false;
+      return true;
     }
   }
-  return includesValue;
+  return false;
 }
--- array-manipulation-2/src/take-right.js
+++ array-manipulation-2/src/take-right.js
@@ -1,1 +1,10 @@
 /* exported takeRight */
+
+/*
+slice(start)
+if start is a negative index, indicates an offset from the end of the sequence
+*/
+
+function takeRight(array, count) {
+  return array.slice(-count);
+}
