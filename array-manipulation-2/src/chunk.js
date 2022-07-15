0fb284053238cb29504374b54b88ef9074ca7b4a
Alice Lee <alicejeeuncodes@gmail.com>
2022-07-15 00:06:45 +0000

    Working on chunk logic to solve optional challenge.

--- array-manipulation-2/src/chunk.js
+++ array-manipulation-2/src/chunk.js
@@ -1,1 +1,16 @@
 /* exported chunk */
+
+/*
+slice(start, end)
+make a new empty array to store chunks
+
+return chunks
+*/
+
+function chunk(array, size) {
+  var chunks = [];
+  for (var i = 0; i < array.length; i += size) {
+    chunks.push(array.slice(i, i + size));
+  }
+  return chunks;
+}
