224d6a505792379e82f58bd8f1fe2c5283719094
Alice Lee <alicejeeuncodes@gmail.com>
2022-07-15 21:35:28 +0000

    Completed array-manipulation-2 exercise.

--- array-manipulation-2/src/chunk.js
+++ array-manipulation-2/src/chunk.js
@@ -2,9 +2,13 @@
 
 /*
 slice(start, end)
-make a new empty array to store chunks
-
+new empty array to store chunks
+make for loop to increase index by size after each iteration
+  start index increases by size for each chunk
+  end index is current index + size
+push to chunks
 return chunks
+*/
 
 function chunk(array, size) {
   var chunks = [];
@@ -13,31 +17,3 @@ function chunk(array, size) {
   }
   return chunks;
 }
-*/
-
-/*
-two empty arrays called chunks and result
-look through array until the end
-Inside our loop, we push to temp using temp.push(arr[a]); if the remainder of a / size is not equal to size - 1.
-Otherwise, we push to temp, push temp to the result variable and reset temp to an empty array.
-Next, if temp isn’t an empty array, we push it to result.
-Return result
-
-function chunk(array, size) {
-  var chunks = [];
-  var result = [];
-  for (var i = 0; i < array.length; i++) {
-    if (i % size !== size - 1) {
-      chunks.push(array[i]);
-    } else {
-      chunks.push(array[i]);
-      result.push(chunks);
-      chunks = [];
-    }
-  }
-  if (chunks.length !== 0) {
-    result.push(chunks);
-  }
-  return result;
-}
-*/
