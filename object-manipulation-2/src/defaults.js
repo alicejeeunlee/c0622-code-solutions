3ee29c6cca8fc85e47597d054d0eb31ba0ee34e9
Alice Lee <alicejeeuncodes@gmail.com>
2022-07-16 01:05:22 +0000

    Working on default function.

--- object-manipulation-2/src/defaults.js
+++ object-manipulation-2/src/defaults.js
@@ -1,1 +1,13 @@
 /* exported defaults */
+
+function defaults(target, source) {
+  for (var key in source) {
+    for (var properties in target) {
+      if (key === properties) {
+        target[key] = target[properties];
+      } else {
+        target[key] = source[key];
+      }
+    }
+  }
+}
--- object-manipulation-2/src/invert.js
+++ object-manipulation-2/src/invert.js
@@ -1,1 +1,18 @@
 /* exported invert */
+
+/*
+make empty object to hold result
+loop through all properties in source(object);
+  set property and value of result
+  result[newProperty] = newValue
+  result[source value] = source key
+return result
+*/
+
+function invert(source) {
+  var result = {};
+  for (var key in source) {
+    result[source[key]] = key;
+  }
+  return result;
+}
