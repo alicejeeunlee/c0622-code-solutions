b647199074d05572aef896564663e7a4520ca131
Alice Lee <alicejeeuncodes@gmail.com>
2022-07-17 20:01:30 +0000

    Completed object-manipulation-2 exercise.

--- object-manipulation-2/src/defaults.js
+++ object-manipulation-2/src/defaults.js
@@ -1,13 +1,18 @@
 /* exported defaults */
 
+/*
+look through all properties(key) in source(object)
+  check if key does not exists in target(object)
+  want condition to evaluate when (key is not found in target => return false)
+  !false = true | condition sastisfied
+    set target property to key and assign value of source property (source[key])
+    target[key] = source[key]
+*/
+
 function defaults(target, source) {
   for (var key in source) {
-    for (var properties in target) {
-      if (key === properties) {
-        target[key] = target[properties];
-      } else {
-        target[key] = source[key];
-      }
+    if (!(key in target)) {
+      target[key] = source[key];
     }
   }
 }
