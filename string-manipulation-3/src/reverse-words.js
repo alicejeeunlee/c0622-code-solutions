923816d123375213461cc82c20256fb73d98b195
Alice Lee <alicejeeuncodes@gmail.com>
2022-07-28 22:09:49 +0000

    Solved reverseWords function.

--- string-manipulation-3/src/reverse-words.js
+++ string-manipulation-3/src/reverse-words.js
@@ -1,19 +1,25 @@
 /* exported reverseWords */
 
-// create an empty string that will store reversed
-// for loop start at last character and decrement
-// concatenate each string[i] into reversed
-// return reversed
+// split string into array of words
+// create empty array to push reversed words
+// loop through words array
+// create empty string to hold reverse word
+// loop through each word backwards
+// concatenate each letter to empty string
+// push reversed word to empty array
+// join array into string
+// return string with words reversed
 
-// function reverseWords(string) {
-//   var words = string.split(' ');
-//   var reversedWords = [];
-//   for (var i = 0; i < words.length; i++) {
-//     var toReverse = '';
-//     for (var k = string.length - 1; k >= 0; k--) {
-//       reversed += string[k];
-//     }
-//   }
-
-//   return reversed;
-// }
+function reverseWords(string) {
+  var words = string.split(' ');
+  var reversedWords = [];
+  for (var i = 0; i < words.length; i++) {
+    var reverse = '';
+    for (var k = words[i].length - 1; k >= 0; k--) {
+      reverse += words[i][k];
+    }
+    reversedWords.push(reverse);
+  }
+  var reversedString = reversedWords.join(' ');
+  return reversedString;
+}
--- string-manipulation-3/src/title-case.js
+++ string-manipulation-3/src/title-case.js
@@ -1,1 +1,15 @@
 /* exported titleCase */
+
+// function titleCase(title) {
+//   var words = title.split(' ');
+//   for (var i = 0; i < words.length; i++) {
+//     for (var k = 1; k < words[i].length; k++) {
+//       if (words[i][0]) {
+//         words[i][0].toUpperCase();
+//       } else {
+//         words[i][k].toLowerCase();
+//       }
+//     }
+//   }
+//   console.log(words);
+// }
