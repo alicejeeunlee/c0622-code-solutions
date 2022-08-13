d2a263a5c3652aac9f8d90d932620f814ea022bf
Alice Lee <alicejeeuncodes@gmail.com>
2022-08-13 01:55:06 +0000

    Updated pseudo-code.

--- object-manipulation-3/main.js
+++ object-manipulation-3/main.js
@@ -1,5 +1,60 @@
 console.log('Lodash is loaded:', typeof _ !== 'undefined');
 
+// create Player constructor function
+// to have property name and hand intiated to empty array (will push cards into hand)
+
+function Player(name) {
+  this.name = name;
+  this.hand = [];
+}
+
+// make function to create deck of cards
+// make deck variable initiated to empty array to hold cards
+// make array of all suits and values possible for card
+// loop through suit
+//   loop through values
+// make card object with property rank and suit
+//   assign rank to values[k]
+//   assign suit to suits[i]
+// push card to deck
+// return deck
+
+function makeDeck() {
+  var deck = [];
+  var suits = ['clubs', 'diamonds', 'hearts', 'spades'];
+  var values = ['ace', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'jack', 'queen', 'king'];
+  for (var i = 0; i < suits.length; i++) {
+    for (var k = 0; k < values.length; k++) {
+      var card = {
+        rank: values[k],
+        suit: suits[i]
+      };
+      deck.push(card);
+    }
+  }
+  return deck;
+}
+
+// create playerList and initiate to empty array
+// loop through array of players (names) to call Player constructor function on each player
+//   push each created player object to playerList
+// create new deck by calling makeDeck fx
+// shuffle deck by calling lodash shuffle method
+// create index to keep track of shuffledDeck[index]
+// loop based on numOfCards to deal (how many loops to run)
+//   loop through each player in PlayerList
+//     push shuffledDeck from index 0 to each player's hand
+//     increment index
+// create data model to keep track of card point system
+// loop through playerList
+//   create score variable initiated to 0 to keep track of total score
+//   add to score based on value of each card in hand
+// initiate winner to first player
+//   loop through player list
+//     if winner score is less than current player score,
+//       reassign winner to current player
+// console.log winner
+
 function playGame(players, numOfCards) {
   var playerList = [];
   for (var i = 0; i < players.length; i++) {
@@ -42,28 +97,7 @@ function playGame(players, numOfCards) {
       winner = playerList[y];
     }
   }
-  console.log(winner.name);
+  console.log('WINNER!', winner.name);
 }
 
 playGame(['Donatello', 'Leonardo', 'Michaelangelo', 'Raphael'], 2);
-
-function Player(name) {
-  this.name = name;
-  this.hand = [];
-}
-
-function makeDeck() {
-  var deck = [];
-  var suits = ['clubs', 'diamonds', 'hearts', 'spades'];
-  var values = ['ace', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'jack', 'queen', 'king'];
-  for (var i = 0; i < suits.length; i++) {
-    for (var k = 0; k < values.length; k++) {
-      var card = {
-        rank: values[k],
-        suit: suits[i]
-      };
-      deck.push(card);
-    }
-  }
-  return deck;
-}
