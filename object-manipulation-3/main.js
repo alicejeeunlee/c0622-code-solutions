c1b02fb8d54b2d3984698ca6d5f7cf48e19cf314
Alice Lee <alicejeeuncodes@gmail.com>
2022-08-13 01:40:11 +0000

    Completed object-manipulation-3 exercise.

--- object-manipulation-3/main.js
+++ object-manipulation-3/main.js
@@ -6,7 +6,43 @@ function playGame(players, numOfCards) {
     playerList.push(new Player(players[i]));
   }
   var deck = makeDeck();
-  return deck;
+  var shuffledDeck = _.shuffle(deck);
+  var index = 0;
+  for (var j = numOfCards; j > 0; j--) {
+    for (var k = 0; k < playerList.length; k++) {
+      playerList[k].hand.push(shuffledDeck[index]);
+      index++;
+    }
+  }
+  console.log(playerList);
+  var cardPoint = {
+    ace: 11,
+    king: 10,
+    queen: 10,
+    jack: 10,
+    10: 10,
+    9: 9,
+    8: 8,
+    7: 7,
+    6: 6,
+    5: 5,
+    4: 4,
+    3: 3,
+    2: 2
+  };
+  for (var x = 0; x < playerList.length; x++) {
+    var score = 0;
+    score += cardPoint[playerList[x].hand[0].rank];
+    score += cardPoint[playerList[x].hand[1].rank];
+    playerList[x].score = score;
+  }
+  var winner = playerList[0];
+  for (var y = 0; y < playerList.length; y++) {
+    if (winner.score < playerList[y].score) {
+      winner = playerList[y];
+    }
+  }
+  console.log(winner.name);
 }
 
 playGame(['Donatello', 'Leonardo', 'Michaelangelo', 'Raphael'], 2);
