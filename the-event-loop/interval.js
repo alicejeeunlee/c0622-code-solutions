1afe00dd5848c733a58c6c1a0b73b5b43a744d84
Alice Lee <alicejeeuncodes@gmail.com>
2022-08-17 21:59:43 +0000

    Fixed simultaneously logging 1 and blast off! to logging after 1 second.

--- the-event-loop/interval.js
+++ the-event-loop/interval.js
@@ -1,9 +1,10 @@
 let count = 3;
 const countdown = setInterval(() => {
-  console.log(count);
-  count--;
   if (count === 0) {
     console.log('Blast off!');
     clearInterval(countdown);
+  } else {
+    console.log(count);
+    count--;
   }
 }, 1000);
