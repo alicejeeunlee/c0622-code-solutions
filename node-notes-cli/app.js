59074c72752ecb36632806f9060a2a989c4230b5
Alice Lee <alicejeeuncodes@gmail.com>
2022-08-18 18:36:08 +0000

    Completed node-notes-cli challenge.

--- node-notes-cli/app.js
+++ node-notes-cli/app.js
@@ -1,7 +1,5 @@
 const fs = require('fs');
 
-console.log(process.argv);
-
 if (process.argv[2] === 'read') {
   fs.readFile('./data.json', 'utf8', (err, data) => {
     if (err) throw err;
@@ -43,9 +41,7 @@ if (process.argv[2] === 'read') {
     const dataModel = require('./data.json');
     const notes = dataModel.notes;
     delete notes[id];
-    console.log(dataModel);
     const JSONdata = JSON.stringify(dataModel, null, 2);
-    console.log(typeof JSONdata, JSONdata);
     fs.writeFile('data.json', JSONdata, err => {
       if (err) throw err;
     });
