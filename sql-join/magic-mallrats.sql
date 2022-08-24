48e0457824d94033e05f9c878237289b0f346c74
Alice Lee <alicejeeuncodes@gmail.com>
2022-08-24 19:49:11 +0000

    Added magic-mallrats.sql.

---
+++ sql-join/magic-mallrats.sql
@@ -0,0 +1,9 @@
+select
+  "customers"."firstName",
+  "customers"."lastName",
+  "films"."title" as "film"
+  from "customers"
+  join "rentals" using ("customerId")
+  join "inventory" using ("inventoryId")
+  join "films" using ("filmId")
+ where "title" = 'Magic Mallrats';
