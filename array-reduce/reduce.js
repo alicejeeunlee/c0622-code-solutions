c63560e54872474c2c74a3106c293234ae7772dd
Alice Lee <alicejeeuncodes@gmail.com>
2022-08-27 00:33:35 +0000

    Working on bonus challenge for array-reduce.

---
+++ array-reduce/reduce.js
@@ -0,0 +1,8 @@
+function reduce(array, reducer, initialValue) {
+  const previousValue = initialValue;
+  const index = 1;
+  const currentValue = array[index - 1];
+  // for (let i = 0; i < array.length; i++) {
+
+  // }
+}
