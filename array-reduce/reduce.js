6175bcd6592c23dd23f0afd0f6b86895ecff45b0
Alice Lee <alicejeeuncodes@gmail.com>
2022-08-29 18:03:51 +0000

    Completed array-reduce bonus challenge.

--- array-reduce/reduce.js
+++ array-reduce/reduce.js
@@ -1,8 +1,37 @@
 function reduce(array, reducer, initialValue) {
-  const previousValue = initialValue;
-  const index = 1;
-  const currentValue = array[index - 1];
-  // for (let i = 0; i < array.length; i++) {
-
-  // }
+  let index = 0;
+  let previous = initialValue;
+  if (initialValue === undefined) {
+    index = 1;
+    previous = array[0];
+  }
+  for (let i = index; i < array.length; i++) {
+    previous = reducer(previous, array[i]);
+  }
+  return previous;
 }
+
+const testSum = reduce([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12], (x, y) => x + y);
+console.log('testSum:', testSum);
+
+const testProduct = reduce([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12], (x, y) => x * y);
+console.log('testProduct', testProduct);
+
+const testBalance = reduce([
+  { type: 'deposit', amount: 150 },
+  { type: 'deposit', amount: 20 },
+  { type: 'withdrawal', amount: 5 },
+  { type: 'deposit', amount: 100 },
+  { type: 'withdrawal', amount: 25 },
+  { type: 'withdrawal', amount: 60 }
+], (x, y) => y.type === 'deposit' ? (x + y.amount) : (x - y.amount), 0);
+console.log('testBalance:', testBalance);
+
+const testComposite = reduce([
+  { color: 'yellow' },
+  { type: 'electric' },
+  { name: 'pikachu' },
+  { level: 15 },
+  { trainer: 'ash' }
+], (x, y) => Object.assign(x, y), {});
+console.log('testComposite:', testComposite);
