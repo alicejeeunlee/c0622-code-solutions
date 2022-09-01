0767d46b3e5cc347e05478954c19a6546f56cc08
Alice Lee <alicejeeuncodes@gmail.com>
2022-09-01 17:27:31 +0000

    Completed react-rendering-lists exercise.

---
+++ react-rendering-lists/src/index.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const pokedex = [
+  { number: '001', name: 'Bulbasaur' },
+  { number: '004', name: 'Charmander' },
+  { number: '007', name: 'Squirtle' },
+  { number: '025', name: 'Pikachu' },
+  { number: '039', name: 'Jigglypuff' }
+];
+
+function List(pokemon) {
+  return <li>{pokemon.value}</li>;
+}
+
+function PokemonList(props) {
+  const listItems = pokedex.map(pokemon =>
+    <List key={pokemon.number.toString()} value={pokemon.name} />
+  );
+  return <ul>{listItems}</ul>;
+}
+
+const root = ReactDOM.createRoot(document.querySelector('#root'));
+root.render(<PokemonList />);
