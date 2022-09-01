b5dd37e90529edff88b2b413b0985f4b76b4c5c5
Alice Lee <alicejeeuncodes@gmail.com>
2022-09-01 03:23:03 +0000

    Completed functionality, working on CSS transitions.

--- react-app-drawer-component/src/app-drawer.jsx
+++ react-app-drawer-component/src/app-drawer.jsx
@@ -4,17 +4,39 @@ class AppDrawer extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      isClicked: false
+      drawerOpen: false
     };
     this.drawer = this.drawer.bind(this);
   }
 
   drawer() {
-
+    this.setState({ drawerOpen: !this.state.drawerOpen });
   }
 
   render() {
-    return <i onClick={this.drawer} className='fa-solid fa-bars'></i>;
+    const modal = this.state.drawerOpen ? '' : 'hidden';
+    return (
+      <div className='container'>
+        <i onClick={this.drawer} className="fa-solid fa-bars"></i>
+        <div className={modal}>
+          <div onClick={this.drawer} className="shadow"></div>
+          <nav className="menu">
+            <h1>Land of Ooo</h1>
+            <div onClick={this.drawer}>
+              <a>Candy Kingdom</a>
+              <a>Ice Kingdom</a>
+              <a>Castle Lemongrab</a>
+              <a>Wildberry Kingdom</a>
+              <a>Lumpy Space</a>
+              <a>Fire Kingdom</a>
+              <a>Cloud Kingdom</a>
+              <a>Evil Forest</a>
+              <a>Bad Lands</a>
+            </div>
+          </nav>
+        </div>
+      </div>
+    );
   }
 }
 
