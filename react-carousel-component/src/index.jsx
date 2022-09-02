43e0b6998d867387be116ac90d4c2b19ba54a07d
Alice Lee <alicejeeuncodes@gmail.com>
2022-09-02 17:56:14 +0000

    Start up files and configurations.

---
+++ react-carousel-component/src/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Carousel from './carousel';
+
+const IMAGES = [];
+
+const root = ReactDOM.createRoot(document.querySelector('#root'));
+root.render(<Carousel images={IMAGES} />);
