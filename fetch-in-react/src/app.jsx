2d6a589d075eafe297b8ad26b2050b41003fa1c9
Alice Lee <alicejeeuncodes@gmail.com>
2022-09-02 23:00:21 +0000

    Updated state within one setState call.

--- fetch-in-react/src/app.jsx
+++ fetch-in-react/src/app.jsx
@@ -14,8 +14,10 @@ class App extends React.Component {
     /* your code here */
     fetch('https://jsonplaceholder.typicode.com/users')
       .then(res => res.json())
-      .then(data => this.setState({ users: data }));
-    this.setState({ isLoading: false });
+      .then(data => this.setState({
+        users: data,
+        isLoading: false
+      }));
   }
 
   render() {
