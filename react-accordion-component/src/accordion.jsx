2dc854bc70bee17e131e6bd7c01d5420f8a8a394
Alice Lee <alicejeeuncodes@gmail.com>
2022-09-02 00:46:24 +0000

    Completed react-accordion-component challenge.

---
+++ react-accordion-component/src/accordion.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+
+export default class Accordion extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      openTab: null
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(event) {
+    if (this.state.openTab === event.target.dataset.topic) {
+      this.setState({ openTab: null });
+    } else {
+      this.setState({ openTab: event.target.dataset.topic });
+    }
+  }
+
+  render() {
+    const reactTopics = [];
+
+    this.props.topics.map(topic => {
+      const hide = this.state.openTab === topic.name ? '' : 'hidden';
+      return reactTopics.push(
+        <div key={this.props.topics.indexOf(topic)}>
+          <h1 onClick={this.handleClick} data-topic={topic.name}>{topic.name}</h1>
+          <p className={hide}>{topic.description}</p>
+        </div>
+      );
+    }
+    );
+
+    return (
+      <div className='container'>
+        {reactTopics}
+      </div>
+    );
+  }
+}
