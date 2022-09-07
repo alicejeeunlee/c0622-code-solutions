783ab3d3aa8211b2831d641271654f19f404ccde
Alice Lee <alicejeeuncodes@gmail.com>
2022-09-07 02:59:33 +0000

    Carousel rotates every 3 seconds.

--- react-carousel-component/src/carousel.jsx
+++ react-carousel-component/src/carousel.jsx
@@ -4,26 +4,41 @@ export default class Carousel extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      imgindex: 0
+      imgIndex: 0,
+      intervalID: null
     };
+    this.dots = this.dots.bind(this);
+  }
+
+  dots(props) {
+    return this.props.images.map((image, index) => {
+      const darken = (this.state.imgIndex === index) ? 'fas' : 'far';
+      return <i key={index} className={`icon ${darken} fa-circle`} data-index={index} />;
+    });
+  }
+
+  componentDidMount() {
+    this.setState({
+      intervalID: setInterval(() => this.setState({
+        imgIndex: this.state.imgIndex === this.props.images.length - 1 ? 0 : this.state.imgIndex + 1
+      }), 3000)
+    });
   }
 
   render() {
+    const images = this.props.images;
+    const index = this.state.imgIndex;
     return (
       <div className="carousel">
         <div className="row space-between align-center">
           <i className="fa-solid fa-less-than fa-2xl arrows"></i>
           <div className='image-wrapper'>
-            <img src="../images/breakfastprincess.webp" alt="" />
+            <img src={`../images/${images[index].url}`} alt={images[index].alt} />
           </div>
           <i className="fa-solid fa-greater-than fa-2xl arrows"></i>
         </div>
         <div className="row space-between icon-wrapper">
-          <i className="icon fas fa-circle" data-index="0"></i>
-          <i className="icon far fa-circle" data-index="1"></i>
-          <i className="icon far fa-circle" data-index="2"></i>
-          <i className="icon far fa-circle" data-index="3"></i>
-          <i className="icon far fa-circle" data-index="4"></i>
+          {this.dots(this.props)}
         </div>
       </div>
     );
--- react-carousel-component/src/index.jsx
+++ react-carousel-component/src/index.jsx
@@ -3,11 +3,11 @@ import ReactDOM from 'react-dom/client';
 import Carousel from './carousel';
 
 const IMAGES = [
-  { url: '../images/princessbubblegum.webp', alt: 'Princess Bubblegum' },
-  { url: '../images/marceline.webp', alt: 'Marceline' },
-  { url: '../images/lumpyspaceprincess.webp', alt: 'Lumpy Space Princess' },
-  { url: '../images/flameprincess.webp', alt: 'Flame Princess' },
-  { url: '../images/breakfastprincess.webp', alt: 'Breakfast Princess' }
+  { url: 'princessbubblegum.webp', alt: 'Princess Bubblegum' },
+  { url: 'marceline.webp', alt: 'Marceline' },
+  { url: 'lumpyspaceprincess.webp', alt: 'Lumpy Space Princess' },
+  { url: 'flameprincess.webp', alt: 'Flame Princess' },
+  { url: 'breakfastprincess.webp', alt: 'Breakfast Princess' }
 ];
 
 const root = ReactDOM.createRoot(document.querySelector('#root'));
