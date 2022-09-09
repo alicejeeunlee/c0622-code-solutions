bea180e43ecb1d722e8f9e97c7f89b1ece2c7211
Alice Lee <alicejeeuncodes@gmail.com>
2022-09-09 01:04:59 +0000

    Completed react-carousel-component challenge.

--- react-carousel-component/src/carousel.jsx
+++ react-carousel-component/src/carousel.jsx
@@ -7,53 +7,50 @@ export default class Carousel extends React.Component {
       imgIndex: 0,
       intervalID: null
     };
-    this.rotate = this.rotate.bind(this);
     this.dots = this.dots.bind(this);
-    // this.handleDotClick = this.dots.bind(this);
-    // this.handleClick = this.handleClick.bind(this);
+    this.handleClick = this.handleClick.bind(this);
   }
 
   dots(props) {
     return this.props.images.map((image, index) => {
       const darken = (this.state.imgIndex === index) ? 'fas' : 'far';
-      return <i key={index} className={`dot ${darken} fa-circle`} data-index={index} onClick={this.handleDotClick}/>;
+      return <i key={index} className={`dot ${darken} fa-circle`} data-index={index} onClick={this.handleClick}/>;
     });
   }
 
-  rotate() {
+  handleClick(event) {
+    clearInterval(this.state.intervalID);
+    if (event.target.id === 'left-arrow') {
+      this.setState({
+        imgIndex: this.state.imgIndex === 0 ? this.props.images.length - 1 : this.state.imgIndex - 1,
+        intervalID: setInterval(() => this.setState({
+          imgIndex: this.state.imgIndex === this.props.images.length - 1 ? 0 : this.state.imgIndex + 1
+        }), 3000)
+      });
+    } else if (event.target.id === 'right-arrow') {
+      this.setState({
+        imgIndex: this.state.imgIndex === this.props.images.length - 1 ? 0 : this.state.imgIndex + 1,
+        intervalID: setInterval(() => this.setState({
+          imgIndex: this.state.imgIndex === this.props.images.length - 1 ? 0 : this.state.imgIndex + 1
+        }), 3000)
+      });
+    } else if (event.target.classList.contains('dot')) {
+      this.setState({ imgIndex: Number(event.target.dataset.index) });
+      clearInterval(this.state.intervalID);
+      this.setState({
+        intervalID: setInterval(() => this.setState({
+          imgIndex: this.state.imgIndex === this.props.images.length - 1 ? 0 : this.state.imgIndex + 1
+        }), 3000)
+      });
+    }
+  }
+
+  componentDidMount() {
     this.setState({
       intervalID: setInterval(() => this.setState({
         imgIndex: this.state.imgIndex === this.props.images.length - 1 ? 0 : this.state.imgIndex + 1
       }), 3000)
     });
-  }
-
-  // handleClick(event) {
-  //   clearInterval(this.state.intervalID);
-  //   if (event.target.id === 'left-arrow') {
-  //     this.setState({
-  //       imgIndex: this.state.imgIndex === 0 ? this.props.images.length - 1 : this.state.imgIndex - 1,
-  //       intervalID: this.rotate()
-  //     });
-  //   } else if (event.target.id === 'right-arrow') {
-  //     this.setState({
-  //       imgIndex: this.state.imgIndex === this.props.images.length - 1 ? 0 : this.state.imgIndex + 1,
-  //       intervalID: this.rotate()
-  //     });
-  //   }
-  //   this.rotate();
-  // }
-
-  // handleDotClick(event) {
-  //   console.log(event.target);
-  //   console.log('clicked!');
-  //   if (event.target.classList.contains('dot')) {
-
-  //   }
-  // }
-
-  componentDidMount() {
-    this.rotate();
   }
 
   render() {
