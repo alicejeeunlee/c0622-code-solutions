bd4b8d4a25e6af159da56e8bf36c81f4b3492b83
Alice Lee <alicejeeuncodes@gmail.com>
2022-09-26 22:15:45 +0000

    Solved get-2nd-from-top.js.

--- data-structures-stacks/get-2nd-from-top.js
+++ data-structures-stacks/get-2nd-from-top.js
@@ -1,5 +1,11 @@
 /* exported get2ndFromTop */
 
 function get2ndFromTop(stack) {
-
+  const top = stack.pop();
+  if (top === undefined) {
+    return undefined;
+  }
+  const secondFromTop = stack.peek();
+  stack.push(top);
+  return secondFromTop;
 }
