093692dbeae1adab4013c9bd77b4f87acb024b59
Alice Lee <alicejeeuncodes@gmail.com>
2022-09-27 22:19:54 +0000

    Solved take-next-smallest.js.

--- data-structures-queues/take-next-smallest.js
+++ data-structures-queues/take-next-smallest.js
@@ -1,5 +1,12 @@
 /* exported takeNextSmallest */
 
 function takeNextSmallest(queue) {
-
+  let first = queue.dequeue();
+  let successor = queue.peek();
+  while (first >= successor) {
+    queue.enqueue(first);
+    first = queue.dequeue();
+    successor = queue.peek();
+  }
+  return first;
 }
