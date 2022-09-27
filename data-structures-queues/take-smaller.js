86ac3ef7fd5585920da4a58b7efc5d841bf9b8d0
Alice Lee <alicejeeuncodes@gmail.com>
2022-09-27 22:04:26 +0000

    Solved take-smaller.js.

--- data-structures-queues/take-smaller.js
+++ data-structures-queues/take-smaller.js
@@ -1,5 +1,17 @@
 /* exported takeSmaller */
 
 function takeSmaller(queue) {
-
+  const first = queue.dequeue();
+  if (queue.peek() === undefined) {
+    return first;
+  } else {
+    const next = queue.dequeue();
+    if (first < next) {
+      queue.enqueue(next);
+      return first;
+    } else {
+      queue.enqueue(first);
+      return next;
+    }
+  }
 }
