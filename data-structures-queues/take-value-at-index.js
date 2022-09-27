bd58033c5160859f330e442d9f6c34a1c27eaee7
Alice Lee <alicejeeuncodes@gmail.com>
2022-09-27 22:07:26 +0000

    Solved take-value-at-index.js.

--- data-structures-queues/take-value-at-index.js
+++ data-structures-queues/take-value-at-index.js
@@ -1,5 +1,13 @@
 /* exported takeValueAtIndex */
 
 function takeValueAtIndex(queue, index) {
-
+  let count = 0;
+  if (queue.peek() === undefined) {
+    return undefined;
+  }
+  while (index !== count) {
+    queue.enqueue(queue.dequeue());
+    count++;
+  }
+  return queue.dequeue();
 }
