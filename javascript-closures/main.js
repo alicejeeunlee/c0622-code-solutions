709d73e160d3f4d6be67d05cb4c9d098f099d47d
Alice Lee <alicejeeuncodes@gmail.com>
2022-09-29 19:20:39 +0000

    Added notes post Q&A.

--- javascript-closures/main.js
+++ javascript-closures/main.js
@@ -1,6 +1,6 @@
 function graduate(credential) {
   return function (fullName) {
-    return `${fullName} ${credential}`;
+    return `${fullName}, ${credential}`;
   };
 }
 
@@ -10,5 +10,20 @@ const lawSchool = graduate('Esq.');
 const doctor = medicalSchool('Alice Lee');
 const lawyer = lawSchool('Alice Lee');
 
+// alt fx call
+// const me = graduate('Software Engineer')('Alice Lee');
+// console.log(me);
+
 console.log(doctor);
 console.log(lawyer);
+
+// ES6
+// const grad = credential => fullName => `${fullName}, ${credential}`;
+// const med = grad('M.D.');
+// const law = grad('Esq.');
+
+// const medGrad = med('Sheldon Cooper');
+// const lawGrad = law('Lisa Simpson');
+
+// console.log(medGrad);
+// console.log(lawGrad);
